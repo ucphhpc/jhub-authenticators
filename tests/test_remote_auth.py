0d9c24374728ee0d8c7317d3f533ef127dc565a4
Rasmus Munk <munk1@live.dk>
2018-06-22 21:30:54 +0200

    update test

--- tests/test_remote_auth.py
+++ tests/test_remote_auth.py
@@ -113,7 +113,7 @@ def test_auth_mount(image, container):
     correct_mig_dict = {'HOST': 'hostaddr',
                         'USERNAME': 'randomstring_unique_string',
                         'PATH': '@host.localhost:',
-                        'MOUNTSSHPRIVATEKEY': '''-----BEGIN RSA PRIVATE KEY-----
+                        'PRIVATEKEY': '''-----BEGIN RSA PRIVATE KEY-----
     MIIEpAIBAAKCAQEA00VP99Nbg6AFrfeByzHtC4G2eLZGDCXP0pBG5tNNmaXKq5sU
     IrDPA7fJczwIfMNlqWeoYjEYg46vbMRxwIDXDDA990JK49+CrpwppxWgSE01WPis
     gtqfmaV16z8CS4WmkjSZnUKQf+2Yk9zdBXOOjWLiXBog7dGpUZQUV/j3u262DIl5

6542e763c0b2cf7118471a0289bf4c8268af8b56
Rasmus Munk <rasmunk@users.noreply.github.com>
2026-02-10 17:01:39 +0100

    Update the default example image

--- examples/jupyterhub_config.py
+++ examples/jupyterhub_config.py
@@ -6,7 +6,7 @@ c.JupyterHub.hub_ip = "0.0.0.0"
 
 c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
 
-c.DockerSpawner.container_image = "nielsbohr/base-notebook:latest"
+c.DockerSpawner.container_image = "ucphhpc/base-notebook:latest"
 c.DockerSpawner.remove_containers = True
 c.DockerSpawner.network_name = "jupyterhub_default"
 # Due a change in how the DockerSpawner escapes usernames in >=12.0.0
